6452a68f21202a304b27225ba2dbaa0c04a3362f
Saurabh <skv222132@gmail.com>
2024-04-15 00:11:13 +0530

    change in login api

--- chatappclient/src/app/login/page.js
+++ chatappclient/src/app/login/page.js
@@ -56,7 +56,8 @@ const Login = () => {
                       className="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline"
                       onClick={() => {
                         window.open(
-                          "http://localhost:5000/auth/google/callback",
+                          // "http://localhost:5000/auth/google/callback",
+                          `${process.env.NEXT_PUBLIC_BASE_URL}/auth/google/callback`,
                           "_self"
                         );
                       }}
